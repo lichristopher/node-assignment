0ce169e5bd7b85c283e7c4060b0476174f1e4bf9
Christopher Li <li.christopher.jr@gmail.com>
2022-05-29 19:52:12 +0800

    change api

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,21 @@
 const button = document.querySelector('button');
 button.addEventListener('click', function () {
-  fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+  fetch('/api')
     .then((response) => response.json())
     .then((data) => {
-      const quote = data[0];
+      console.log(data);
+      const quote = data.quote;
+      const author = data.author;
       const h1 = document.querySelector('h1');
+      const h2 = document.querySelector('h2');
       h1.textContent = `"${quote}"`;
+      h2.textContent = `${author}`;
     });
+  // fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     const quote = data[0];
+  //     const h1 = document.querySelector('h1');
+  //     h1.textContent = `"${quote}"`;
+  //   });
 });
