4dc5a79862fcbaef0b8a6b28ffdcde9c2163cc69
Aidan Williams <aidanw4@gmail.com>
2022-05-29 16:13:33 +0700

    add quote to quote

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const button = document.querySelector('button');
-button.addEventListener('click', function () {
-  fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+const button = document.querySelector("button");
+button.addEventListener("click", function () {
+  fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
     .then((response) => response.json())
     .then((data) => {
       const quote = data[0];
-      const h1 = document.querySelector('h1');
-      h1.textContent = quote;
+      const h1 = document.querySelector("h1");
+      h1.textContent = `"${quote}"`;
     });
 });
